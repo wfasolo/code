a9b9ae6a6076265a434bbb80ef03f95b668b4bb0
wfasolo <wfasolo@gmail.com>
2023-11-06 21:07:56 -0300

    cedae

--- programmer/programmer/cedae/Cedae_Vazao/leituras.ino
+++ programmer/programmer/cedae/Cedae_Vazao/leituras.ino
@@ -11,9 +11,12 @@ void ler_vazao() {
   float distance = duration * pulso / 2;
 
   altura = dist - distance;
-  md_ler[1] = md_ler[0];
-  md_ler[2] = md_ler[1];
+  if (altura < 0) {
+    altura = md_ler[0];
+  }
   md_ler[3] = md_ler[2];
+  md_ler[2] = md_ler[1];
+  md_ler[1] = md_ler[0];
   altura = (altura + md_ler[0] + md_ler[1] + md_ler[2] + md_ler[3]) / 5;
   md_ler[0] = altura;
   vazao = 0.69 * pow(altura, 1.522);
