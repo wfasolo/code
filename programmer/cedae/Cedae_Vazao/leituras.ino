f50b649ac414728150e5c4b0e1b6810fad289ba7
wfasolo <48557664+wfasolo@users.noreply.github.com>
2025-02-16 17:36:24 -0300

    Update leituras.ino

--- programmer/cedae/Cedae_Vazao/leituras.ino
+++ programmer/cedae/Cedae_Vazao/leituras.ino
@@ -36,3 +36,5 @@ void ler_vazao()
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
+    // Aplicação do filtro de Kalman adaptativo para suavizar a medição
+    // float filteredHeight = kalmanFilterAdaptive(measuredHeight);
